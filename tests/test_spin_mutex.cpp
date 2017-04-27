58bbf09f6ca34a452306578db1a25a9338ed91d1
xiongziliang <771730766@qq.com>
2017-04-27 17:30:14 +0800

    add test for spin_mutex

---
+++ tests/test_spin_mutex.cpp
@@ -0,0 +1,77 @@
+//============================================================================
+// Name        : ToolKitTest.cpp
+// Author      : xzl
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+#include <signal.h>
+#include <unistd.h>
+#include <iostream>
+#include "Util/logger.h"
+#include "Thread/threadgroup.h"
+#include "Thread/semaphore.h"
+#include "Thread/spin_mutex.h"
+#include <list>
+
+using namespace std;
+using namespace ZL::Util;
+using namespace ZL::Thread;
+
+bool g_bExitFlag = false;
+semaphore g_sem;
+spin_mutex g_mtxList;
+list<int> g_list;
+
+void programExit(int arg) {
+	g_bExitFlag = true;
+	for(int i = 0 ; i < 10 ;++i){
+		//let consumer threads exit!
+		lock_guard<spin_mutex> lck(g_mtxList);
+		g_list.push_front(i++);
+		g_sem.post();
+	}
+}
+
+void onConsum(int index) {
+	while (!g_bExitFlag) {
+		g_sem.wait();
+		lock_guard<spin_mutex> lck(g_mtxList);
+		DebugL << "thread " << index << ":" << g_list.back() << endl;
+		g_list.pop_back();
+	}
+}
+void onProduce() {
+	int i = 0;
+	while (!g_bExitFlag) {
+		usleep(1000); // sleep 1 ms
+		lock_guard<spin_mutex> lck(g_mtxList);
+		g_list.push_front(i++);
+		g_list.push_front(i++);
+		g_list.push_front(i++);
+		g_list.push_front(i++);
+		g_sem.post(4);
+	}
+}
+int main() {
+	signal(SIGINT, programExit);
+	Logger::Instance().add(std::make_shared<ConsoleChannel>("stdout", LTrace));
+
+	thread_group thread_producer;
+	thread_producer.create_thread([]() {
+		//生产者
+		onProduce();
+	});
+
+	thread_group thread_consumer;
+	for (int i = 0; i < 4; ++i) {
+		thread_consumer.create_thread([i]() {
+			//消费者
+			onConsum(i);
+		});
+	}
+	thread_consumer.join_all();
+	thread_producer.join_all();
+	Logger::Destory();
+	return 0;
+}
