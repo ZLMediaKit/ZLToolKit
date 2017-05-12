daca88a8482a5a4f7f813889a31a086c7ba47aa9
xzl <771730766@qq.com>
2017-05-12 16:07:16 +0800

    arm平台默认关闭自旋锁

--- src/Thread/spin_mutex.h
+++ src/Thread/spin_mutex.h
@@ -9,11 +9,13 @@
 #define SRC_THREAD_SPIN_MUTEX_H_
 
 #include <atomic>
-
+#include <mutex>
 using namespace std;
 
 namespace ZL {
 namespace Thread {
+
+#ifndef __ARM_ARCH
 
 class spin_mutex {
   std::atomic_flag flag = ATOMIC_FLAG_INIT;
@@ -22,13 +24,16 @@ public:
   spin_mutex(const spin_mutex&) = delete;
   spin_mutex& operator= (const spin_mutex&) = delete;
   void lock() {
-    while(flag.test_and_set(std::memory_order_acquire))
-      ;
+    while(flag.test_and_set(std::memory_order_acquire)) ;
   }
   void unlock() {
     flag.clear(std::memory_order_release);
   }
 };
+
+#else
+	typedef mutex spin_mutex;
+#endif //__ARM_ARCH
 
 } /* namespace Thread */
 } /* namespace ZL */
