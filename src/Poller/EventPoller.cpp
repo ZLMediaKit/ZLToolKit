b7f3bd79876420c815ca6816e12d24e2afab7d71
xzl <771730766@qq.com>
2017-06-23 13:37:48 +0800

    修复主线程异常导致的崩溃

--- src/Poller/EventPoller.cpp
+++ src/Poller/EventPoller.cpp
@@ -45,10 +45,10 @@ EventPoller::EventPoller(bool enableSelfRun) {
 	SockUtil::setNoBlocked(pipe_fd[1],false);
 #ifdef HAS_EPOLL
 
-#if defined(__ARM_ARCH) || defined(ANDROID)
-    epoll_fd = epoll_create(1024);
-#else
+#ifndef __ARM_ARCH
 	epoll_fd = epoll_create1(EPOLL_CLOEXEC);
+#else
+	epoll_fd = epoll_create(1024);
 #endif
 
 	if (epoll_fd == -1) {
@@ -224,7 +224,11 @@ inline Sigal_Type EventPoller::_handlePipeEvent(uint64_t type, uint64_t i64_size
 	switch (type) {
 	case Sig_Async: {
 		PollAsyncCB **cb = (PollAsyncCB **) buf;
-		(*cb)->operator()();
+		try{
+			(*cb)->operator()();
+		}catch (std::exception &ex) {
+			FatalL << ex.what();
+		}
 		delete *cb;
 	}
 		break;
