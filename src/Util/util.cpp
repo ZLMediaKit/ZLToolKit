f0b0375490797d77a18d808abec3a8dfa400fa54
xiongziliang <771730766@qq.com>
2017-09-27 11:29:09 +0800

    mac下获取应用程序路径

--- src/Util/util.cpp
+++ src/Util/util.cpp
@@ -22,6 +22,43 @@
 #pragma comment(lib, "shlwapi.lib")
 #endif // defined(_WIN32)
 
+
+#if defined(__MACH__) || defined(__APPLE__)
+#include <mach/mach.h>
+#include <mach/mach_time.h>
+#include <mach-o/dyld.h> /* _NSGetExecutablePath */
+int uv_exepath(char* buffer, int *size) {
+	/* realpath(exepath) may be > PATH_MAX so double it to be on the safe side. */
+	char abspath[PATH_MAX * 2 + 1];
+	char exepath[PATH_MAX + 1];
+	uint32_t exepath_size;
+	size_t abspath_size;
+
+	if (buffer == NULL || size == NULL || *size == 0)
+		return -EINVAL;
+
+	exepath_size = sizeof(exepath);
+	if (_NSGetExecutablePath(exepath, &exepath_size))
+		return -EIO;
+
+	if (realpath(exepath, abspath) != abspath)
+		return -errno;
+
+	abspath_size = strlen(abspath);
+	if (abspath_size == 0)
+		return -EIO;
+
+	*size -= 1;
+	if (*size > abspath_size)
+		*size = abspath_size;
+
+	memcpy(buffer, abspath, *size);
+	buffer[*size] = '\0';
+
+	return 0;
+}
+#endif //defined(__MACH__) || defined(__APPLE__)
+
 using namespace std;
 
 namespace ZL {
@@ -74,25 +111,38 @@ string hexdump(const void *buf, size_t len) {
 	}
 	return ret;
 }
+
 static string _exePath("./");
 string exePath() {
-	string filePath;
-	char buffer[1024] = {0};
+	char buffer[PATH_MAX * 2 + 1] = {0};
+	int n = -1;
 #if defined(_WIN32)
-	int n = GetModuleFileNameA(NULL, buffer, sizeof(buffer));
-#else
-	int n = readlink("/proc/self/exe", buffer, sizeof(buffer));
-#endif //WIN32
+	n = GetModuleFileNameA(NULL, buffer, sizeof(buffer));
+#elif defined(__MACH__) || defined(__APPLE__)
+	n = sizeof(buffer);
+   	if(uv_exepath(buffer,&n) !=0 ){
+		n = -1;
+	}
+#elif defined(__linux__)
+	n = readlink("/proc/self/exe", buffer, sizeof(buffer));
+#endif
+
+	string filePath;
 	if (n <= 0) {
 		filePath = _exePath;
 	} else {
 		filePath = buffer;
 	}
+
+#if defined(_WIN32)
+    //windows下把路径统一转换层unix风格，因为后续都是按照unix风格处理的
 	for (auto &ch : filePath) {
 		if (ch == '\\') {
 			ch = '/';
 		}
 	}
+#endif //defined(_WIN32)
+
 	return filePath;
 }
 void setExePath(const string &path){
--- src/Util/util.h
+++ src/Util/util.h
@@ -49,6 +49,7 @@ string hexdump(const void *buf, size_t len);
 string exePath();
 string exeDir();
 string exeName();
+//设置应用程序文件路径，非win/mac/linux系统才有效
 void setExePath(const string &path);
 
 #ifndef bzero
