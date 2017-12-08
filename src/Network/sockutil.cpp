bfea43a6d747621a9274063d5bc9f46af6016480
xiongziliang <771730766@qq.com>
2017-12-08 20:02:05 +0800

    修复IOS、MAC下获取有效IP失败的BUG

--- src/Network/sockutil.cpp
+++ src/Network/sockutil.cpp
@@ -292,10 +292,11 @@ string getIosIp(){
         temp_addr = interfaces;
         while(temp_addr != NULL) {
             if(temp_addr->ifa_addr->sa_family == AF_INET) {
-                // Check if interface is en0 which is the wifi connection on the iPhone
-                if(strcmp(temp_addr->ifa_name,"en0")==0) {
-                    // Get NSString from C String
-                    address = inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr);
+				string tmp = inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr);
+                if(tmp != "127.0.0.1" && tmp != "0.0.0.0") {
+					//获取一个有效IP
+					address = tmp;
+					break;
                 }
             }
             temp_addr = temp_addr->ifa_next;
