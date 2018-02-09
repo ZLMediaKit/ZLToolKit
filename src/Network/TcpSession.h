50f8dbdb6641e46849d34c92148610d26dc7a8df
xiongziliang <771730766@qq.com>
2018-02-09 15:11:22 +0800

    暴露几个方法

--- src/Network/TcpSession.h
+++ src/Network/TcpSession.h
@@ -46,8 +46,7 @@ public:
 		localPort = sock->get_local_port();
 		peerPort = sock->get_peer_port();
 	}
-	virtual ~TcpSession() {
-	}
+	virtual ~TcpSession() {}
 	virtual void onRecv(const Socket::Buffer::Ptr &) =0;
 	virtual void onError(const SockException &err) =0;
 	virtual void onManager() =0;
@@ -60,31 +59,30 @@ public:
 	void async_first(T &&task) {
 		th->async_first(std::forward<T>(task));
 	}
-
+    void safeShutdown(){
+        std::weak_ptr<TcpSession> weakSelf = shared_from_this();
+        async_first([weakSelf](){
+            auto strongSelf = weakSelf.lock();
+            if(strongSelf){
+                strongSelf->shutdown();
+            }
+        });
+    }
+    const string& getLocalIp() const {
+        return localIp;
+    }
+    const string& getPeerIp() const {
+        return peerIp;
+    }
+    uint16_t getLocalPort() const {
+        return localPort;
+    }
+    uint16_t getPeerPort() const {
+        return peerPort;
+    }
 protected:
-	const string& getLocalIp() const {
-		return localIp;
-	}
-	const string& getPeerIp() const {
-		return peerIp;
-	}
-	uint16_t getLocalPort() const {
-		return localPort;
-	}
-	uint16_t getPeerPort() const {
-		return peerPort;
-	}
 	virtual void shutdown() {
 		sock->emitErr(SockException(Err_other, "self shutdown"));
-	}
-	void safeShutdown(){
-		std::weak_ptr<TcpSession> weakSelf = shared_from_this();
-		async_first([weakSelf](){
-			auto strongSelf = weakSelf.lock();
-			if(strongSelf){
-				strongSelf->shutdown();
-			}
-		});
 	}
 	virtual int send(const string &buf) {
 		return sock->send(buf);
@@ -98,7 +96,6 @@ protected:
 	virtual int send(const Socket::Buffer::Ptr &buf) {
 		return sock->send(buf);
 	}
-
 	Socket::Ptr sock;
 private:
 	std::shared_ptr<ThreadPool> th;
