6ab5cfeebad2f7377d890eae3540ae0df48968e5
xiongziliang <771730766@qq.com>
2018-05-11 16:40:55 +0800

    openssl 适配

--- src/Util/SSLBox.h
+++ src/Util/SSLBox.h
@@ -26,13 +26,16 @@
 #define CRYPTO_SSLBOX_H_
 
 #include <fcntl.h>
-#include <openssl/bio.h>
-#include <openssl/ossl_typ.h>
 #include <mutex>
 #include <string>
 #include <atomic>
 #include <functional>
 #include "logger.h"
+
+#if defined(ENABLE_OPENSSL)
+
+#include <openssl/bio.h>
+#include <openssl/ossl_typ.h>
 
 #if defined(_WIN32)
 #if defined(_WIN64)
@@ -60,12 +63,14 @@
 #endif //defined(_WIN64)
 #endif // defined(_WIN32)
 
+#endif // defined(ENABLE_OPENSSL)
 
 using namespace std;
 
 namespace ZL {
 namespace Util {
 
+#if defined(ENABLE_OPENSSL)
 class SSL_Initor {
 public:
 	friend class SSL_Box;
@@ -84,8 +89,8 @@ private:
 	void setCtx(SSL_CTX *ctx);
 	void loadPem(SSL_CTX *ctx, const char *keyAndCA_pem,const char *import_pwd);
 	inline std::string getLastError();
-}
-;
+};
+
 class SSL_Box {
 public:
 	SSL_Box(bool isServer = true, bool enable = true);
@@ -122,6 +127,59 @@ private:
 	void flushReadBio(char *buf, int bufsize);
 
 };
+
+#else
+class SSL_Initor {
+public:
+	friend class SSL_Box;
+	static SSL_Initor &Instance() {
+		static SSL_Initor obj;
+		return obj;
+	}
+	void loadServerPem(const char *keyAndCA_pem, const char *import_pwd = ""){};
+	void loadClientPem(const char *keyAndCA_pem, const char *import_pwd = ""){};
+private:
+	SSL_Initor(){};
+	~SSL_Initor(){};
+};
+
+
+class SSL_Box {
+public:
+	SSL_Box(bool isServer = true, bool enable = true){
+		if(enable){
+			WarnL << "openssl disabled!";
+		}
+	};
+	virtual ~SSL_Box(){};
+
+	//收到密文后，调用此函数解密
+	void onRecv(const char *data, uint32_t data_len){
+		onDec(data,data_len);
+	};
+	//需要加密明文调用此函数
+	void onSend(const char *data, uint32_t data_len){
+		onEnc(data,data_len);
+	};
+
+	//设置解密后获取明文的回调
+	template<typename F>
+	void setOnDecData(F &&fun) {
+		onDec = std::forward<F>(fun);
+	}
+
+	//设置加密后获取密文的回调
+	template<typename F>
+	void setOnEncData(F &&fun) {
+		onEnc = std::forward<F>(fun);;
+	}
+	void shutdown(){};
+private:
+	function<void(const char *data, uint32_t len)> onDec;
+	function<void(const char *data, uint32_t len)> onEnc;
+};
+#endif //defined(ENABLE_OPENSSL)
+
 
 } /* namespace Util */
 } /* namespace ZL */
