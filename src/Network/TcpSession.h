ca6ab8b4c406c3c2e91ca0979c843908d1edb4bd
xiongziliang <771730766@qq.com>
2020-02-13 11:55:49 +0800

    完善对象调试打印

--- src/Network/TcpSession.h
+++ src/Network/TcpSession.h
@@ -95,11 +95,11 @@ private:
 	SSL_Box _sslBox;
 };
 
-#define TraceP(ptr) TraceL << ptr << "(" << ptr->get_peer_ip() << ":" << ptr->get_peer_port() << ") "
-#define DebugP(ptr) DebugL << ptr << "(" << ptr->get_peer_ip() << ":" << ptr->get_peer_port() << ") "
-#define InfoP(ptr) InfoL << ptr << "(" << ptr->get_peer_ip() << ":" << ptr->get_peer_port() << ") "
-#define WarnP(ptr) WarnL << ptr << "(" << ptr->get_peer_ip() << ":" << ptr->get_peer_port() << ") "
-#define ErrorP(ptr) ErrorL << ptr << "(" << ptr->get_peer_ip() << ":" << ptr->get_peer_port() << ") "
+#define TraceP(ptr) TraceL << ptr->getIdentifier() << "(" << ptr->get_peer_ip() << ":" << ptr->get_peer_port() << ") "
+#define DebugP(ptr) DebugL << ptr->getIdentifier() << "(" << ptr->get_peer_ip() << ":" << ptr->get_peer_port() << ") "
+#define InfoP(ptr) InfoL << ptr->getIdentifier() << "(" << ptr->get_peer_ip() << ":" << ptr->get_peer_port() << ") "
+#define WarnP(ptr) WarnL << ptr->getIdentifier() << "(" << ptr->get_peer_ip() << ":" << ptr->get_peer_port() << ") "
+#define ErrorP(ptr) ErrorL << ptr->getIdentifier() << "(" << ptr->get_peer_ip() << ":" << ptr->get_peer_port() << ") "
 
 
 } /* namespace toolkit */
