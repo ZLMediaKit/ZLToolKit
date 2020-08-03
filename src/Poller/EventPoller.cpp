af531561e20fe4ba43431a4802ee399b6b7e7657
Lucas Zanella <me@lucaszanella.com>
2020-08-03 02:08:21 +0000

    fixes event poller

--- src/Poller/EventPoller.cpp
+++ src/Poller/EventPoller.cpp
@@ -67,12 +67,6 @@ EventPoller::EventPoller(ThreadPool::Priority priority ) {
     }
 }
 
-void EventPoller::doTaskOnPollerThread(function<void()> &&task) {
-    async_first([task = std::move(task)](){
-        task();
-    });
-}
-
 void EventPoller::shutdown() {
     async_l([](){
         throw ExitException();
