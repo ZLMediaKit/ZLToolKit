1b6fff04764bee1f8c731073110b51a2daa184ab
xiongziliang <771730766@qq.com>
2021-01-24 21:53:31 +0800

    修复ssize_t重定义问题

--- src/Util/util.h
+++ src/Util/util.h
@@ -32,6 +32,8 @@
 #else
 #include <unistd.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <stddef.h>
 #endif // defined(_WIN32)
 
 #if defined(__APPLE__)
@@ -204,10 +206,6 @@ bool end_with(const string &str, const string &substr);
 #define bzero(ptr,size)  memset((ptr),0,(size));
 #endif //bzero
 
-#ifndef ssize_t
-#define ssize_t int64_t
-#endif
-
 #if defined(ANDROID)
 template <typename T>
 std::string to_string(T value){
@@ -226,6 +224,9 @@ int asprintf(char **strp, const char *fmt, ...);
 #define strcasecmp _stricmp
 #endif
 const char *strcasestr(const char *big, const char *little);
+#ifndef ssize_t
+#define ssize_t int64_t
+#endif
 #endif //WIN32
 
 /**
