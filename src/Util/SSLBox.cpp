c0675bc2cdb77759abe1819e9083ce7b7657fb48
Marhoosh <54924700+Marhoosh@users.noreply.github.com>
2024-02-23 10:55:19 +0800

    禁用SSLv2、SSLv3、TLSv1、TLSv1.1不安全协议

--- src/Util/SSLBox.cpp
+++ src/Util/SSLBox.cpp
@@ -210,6 +210,12 @@ void SSL_Initor::setupCtx(SSL_CTX *ctx) {
 #ifdef SSL_OP_NO_RENEGOTIATION /* openssl 1.1.0 */
     ssloptions |= SSL_OP_NO_RENEGOTIATION;
 #endif
+
+    ssloptions |= SSL_OP_NO_SSLv2;
+    ssloptions |= SSL_OP_NO_SSLv3;
+    ssloptions |= SSL_OP_NO_TLSv1;
+    ssloptions |= SSL_OP_NO_TLSv1_1;
+    
     SSL_CTX_set_options(ctx, ssloptions);
 
 #endif //defined(ENABLE_OPENSSL)
