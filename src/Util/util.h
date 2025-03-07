3cf880eee300b08a6eb3aad90c25dd2a3d0551f1
LWC <45446340+linwenchen@users.noreply.github.com>
2025-03-07 11:28:23 +0800

    [fix] 修复 toolkit::Any::empty() 返回always true 问题(#263) (#265)

--- src/Util/util.h
+++ src/Util/util.h
@@ -472,7 +472,7 @@ public:
     }
 
     operator bool() const { return _data.operator bool(); }
-    bool empty() const { return !bool(); }
+    bool empty() const { return !operator bool(); }
 
     void reset() {
         _type = nullptr;
